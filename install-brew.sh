5bf8001e204a630b44e063c0d2171f535fa69279
Suneet Misra <hello@suneet.codes>
2024-07-25 17:47:14 +0100

    adds instructions to install brew tools and casks

--- install-brew.sh
+++ install-brew.sh
@@ -9,3 +9,54 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # update brew and verify readiness
 brew update && brew doctor
+
+
+# use brew to install tools and applications
+
+CLIS=(
+  git
+  tree
+  zsh
+)
+
+CASKS=(
+  1password
+  ankerwork
+  appcleaner
+  arc
+  betterdisplay
+  brave-browser
+  devonthink
+  discord
+  docker
+  expressvpn
+  figma
+  firefox
+  font-fira-code-nerd-font
+  fujitsu-scansnap-home
+  google-chrome
+  handbrake
+  iterm2
+  keyboardcleantool
+  linear-linear
+  logi-options-plus
+  microsoft-auto-update
+  microsoft-edge
+  microsoft-teams
+  notion
+  numi
+  obsidian
+  ollama
+  postman
+  raycast
+  spotify
+  transmission
+  visual-studio-code
+  vlc
+  warp
+  whatsapp
+  zoom
+)
+
+brew install ${CLIS[@]}
+brew install --cask ${CASKS[@]}
